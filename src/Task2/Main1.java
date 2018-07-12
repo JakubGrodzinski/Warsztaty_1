c42e115888715882af4f28a23dcbd9de3e58756b
Jakub Grodzinski <jakubgrodzinski@protonmail.com>
2018-07-12 09:31:11 -0400

    ComputerGuesses

--- src/Task2/Main1.java
+++ src/Task2/Main1.java
@@ -1,4 +1,117 @@
 package src.Task2;
 
-public class Main1 {
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main1
+{
+    public static void main (String[] args)
+    {
+
+        int[] userNum = sortNumbers(getNumbers());
+        int[] genNum = sortNumbers(randomNumberGenerator());
+        printNumbers(userNum);
+        printNumbers(genNum);
+        win(atLeastThree(userNum, genNum));
+    }
+
+    static int[] getNumbers()
+    {
+        Scanner sc = new Scanner(System.in);
+        int[] userNumbers = new int[6];
+        for(int i = 0; i < 6; i++)
+        {
+            System.out.println("Wpisz typowaną liczbę (liczba nr: " + (i + 1) + ")");
+            try
+            {
+                userNumbers[i] = Integer.parseInt(sc.nextLine());
+            }
+            catch (NumberFormatException e)
+            {
+                System.out.println("To nie jest liczba");
+                i--;
+                continue;
+            }
+            for(int j = 0; j < i; j++)
+            {
+                if(userNumbers[j] == userNumbers[i])
+                {
+                    System.out.println("Liczba wpisana poprzednio");
+                    i--;
+                }
+            }
+            if(userNumbers[i] < 1 || userNumbers[i] > 49)
+            {
+                System.out.println("Liczba spoza zakresu");
+                i--;
+            }
+        }
+        return userNumbers;
+    }
+
+    static int[] sortNumbers (int[] numbers)
+    {
+         Arrays.sort(numbers);
+         return numbers;
+    }
+    static void printNumbers (int[] numbers)
+    {
+        for(int i = 0; i < 6; i++)
+        {
+            System.out.print(numbers[i] + " ");
+            System.out.println();
+        }
+    }
+    static int[] randomNumberGenerator ()
+    {
+        int[] rng = new int[6];
+        Integer[] arr = new Integer[49];
+        for (int i = 0; i < arr.length; i++)
+        {
+            arr[i] = i + 1;
+        }
+        Collections.shuffle(Arrays.asList(arr));
+        for (int i = 0; i < 6; i++)
+        {
+            rng[i] = arr[i];
+        }
+        return rng;
+    }
+    static boolean atLeastThree (int[] userNumbers, int[] randomNumbers)
+    {
+        int i = 0;
+        int j = 0;
+        int howMany = 0;
+        while (i != 6 && j!= 6)
+        {
+            if(userNumbers[i] == randomNumbers[j])
+            {
+                howMany++;
+                i++;
+                j++;
+                if(howMany > 2)
+                {
+                    return true;
+                }
+
+            }
+            else if (userNumbers[i] > randomNumbers[j])
+            {
+                j++;
+            }
+            else
+            {
+                i++;
+            }
+        }
+        return false;
+    }
+    static void win (boolean three)
+    {
+        if (three)
+        {
+            System.out.println("Trafiłeś!");
+        }
+    }
 }
--- src/Task3/Main3.java
+++ src/Task3/Main3.java
@@ -1,4 +1,79 @@
 package src.Task3;
 
-public class Main3 {
+import java.util.Scanner;
+
+public class Main3
+{
+    public static void main(String[] args)
+    {
+        computerGuesses();
+    }
+
+    static void computerGuesses ()
+    {
+
+        int guess = 0;
+        String userResponse;
+        int min = 0;
+        int max = 1000;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Pomyśl liczbę od 0 do 1000, a ja zgadnę ją w max. 10 próbach");
+
+        while(true)
+        {
+
+                guess = (min + max) / 2;
+                System.out.println("Zgaduję:" + guess);
+
+            System.out.println("Zgadłem? (tak/nie)");
+            userResponse = sc.nextLine();
+            if(!userResponse.equals("tak") && !userResponse.equals("nie"))
+            {
+                System.out.println("Wpisz \"tak\" lub \"nie\"");
+
+            }
+            else if (userResponse.equals("tak"))
+            {
+                System.out.println("Wygrałem!");
+                break;
+            }
+            else
+            {
+                System.out.println("Za dużo? (tak/nie)");
+                userResponse = sc.nextLine();
+                if(!userResponse.equals("tak") && !userResponse.equals("nie"))
+                {
+                    System.out.println("Wpisz \"tak\" lub \"nie\"");
+
+                }
+                else if (userResponse.equals("tak"))
+                {
+                    max = guess;
+                }
+                else
+                {
+                    System.out.println("Za mało? (tak/nie)");
+                    userResponse = sc.nextLine();
+                    if(!userResponse.equals("tak") && !userResponse.equals("nie"))
+                    {
+                        System.out.println("Wpisz \"tak\" lub \"nie\"");
+
+                    }
+                    else if (userResponse.equals("tak"))
+                    {
+                        min = guess;
+                    }
+                    else
+                    {
+                        System.out.println("Nie oszukuj!");
+                    }
+
+                }
+
+            }
+
+
+        }
+    }
 }
